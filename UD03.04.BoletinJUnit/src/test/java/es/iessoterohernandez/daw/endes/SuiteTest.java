7b162c72fae861624e3b38cb396284b8ee2157b1
Jose L. Vital <151146472+jlvital@users.noreply.github.com>
2024-03-01 19:01:59 +0100

    Update and rename TestBadMoney.java to SuiteTest.java

---
+++ UD03.04.BoletinJUnit/src/test/java/es/iessoterohernandez/daw/endes/SuiteTest.java
@@ -0,0 +1,7 @@
+package es.iessoterohernandez.daw.endes;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+	@SelectPackages({"es.iessoterohernandez.daw.endes"})
+	@Suite
+	public class SuiteTest {}
