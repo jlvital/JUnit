a568589061af51bdd1f12c5ed1cfe274b39a0bc4
Jose L. Vital <151146472+jlvital@users.noreply.github.com>
2024-03-01 18:35:55 +0100

    Update TestFridge.java
    Actualización test

--- UD03.04.BoletinJUnit/src/test/java/es/iessoterohernandez/daw/endes/TestFridge.java
+++ UD03.04.BoletinJUnit/src/test/java/es/iessoterohernandez/daw/endes/TestFridge.java
@@ -1,5 +1,45 @@
 package es.iessoterohernandez.daw.endes;
 
-public class TestFridge {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class FridgeTest {
+	
+	private Fridge frigo;
+	@BeforeEach
+	public void testInicial() {
+		frigo = new Fridge();
+	}
+
+	@AfterEach
+	public void testFinal() {
+		frigo = null;
+	}
+    @ParameterizedTest
+    @ValueSource(strings = {"yogurt", "batido", "leche"})
+    void testPut(String item) {
+    }
+
+    @Test
+    void testContains() {
+        String item = "tomate";
+        assertFalse(frigo.contains(item));
+        frigo.put(item);
+        assertTrue(frigo.contains(item));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"yogurt", "batido", "leche"})
+    void testTake(String item) {
+        try {
+			frigo.take(item);
+		} catch (NoSuchItemException e) {
+			
+		}
+    }
 }
