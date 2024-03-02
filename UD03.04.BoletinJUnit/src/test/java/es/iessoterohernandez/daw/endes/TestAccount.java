dbb508a222d5af22d403d4eeb62adc83ca657d25
Jose L. Vital <151146472+jlvital@users.noreply.github.com>
2024-03-02 10:15:55 +0100

    Update TestAccount.java

--- UD03.04.BoletinJUnit/src/test/java/es/iessoterohernandez/daw/endes/TestAccount.java
+++ UD03.04.BoletinJUnit/src/test/java/es/iessoterohernandez/daw/endes/TestAccount.java
@@ -1,20 +1,65 @@
 package es.iessoterohernandez.daw.endes;
 
-import static org.junit.Assert.assertTrue;
+import java.text.NumberFormat;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
-/**
- * Unit test for simple App.
- */
-public class TestAccount 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+public class AccountTest {
+	private static Account account;
+
+	@BeforeEach
+	public void testInicial() {
+		account = new Account("Jose", 359359359, 500);
+	}
+
+	@AfterEach
+	public void testFinal() {
+		account = null;
+	}
+
+	@Test
+	public void testDepositoDinero() {
+
+		assertTrue(account.deposit(500));
+		assertEquals(1000, account.getBalance());
+		assertFalse(account.deposit(180));
+	}
+
+	@Test
+	public void testRetiradaEfectivo() {
+		assertTrue(account.withdraw(300, 0));
+		assertEquals(700, account.getBalance());
+	}
+
+	@Test
+	public void testNumeroCuenta() {
+		assertEquals(359359359, account.getAccountNumber());
+	}
+
+	@Test
+	public void testTipoInteres() {
+		float initialBalance = account.getBalance();
+		account.addInterest();
+		float finalBalance = account.getBalance();
+		assertEquals(104.5f, finalBalance);
+	}
+
+	@Test
+	public void testDecimalFailure() {
+		float balance = account.getBalance();
+		for (int i = 0; i < 100; i++) {
+			balance += 0.23f;
+		}
+		assertNotEquals(balance, 773f);
+
+	}
+
+	@Test
+	public void testToString()
     {
-        assertTrue( true );
-    }
+        assertEquals("359359359\tJose\t500 €", account.toString());
+}
 }
